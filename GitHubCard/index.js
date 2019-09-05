f6b5cc263179b69e6c68181747b2a1e7fba38e18
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-09-05 09:34:17 -0400

    initial commit, did step 1 in index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,11 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/Lfritze')
+  .then((response) => {
+    console.log(response);
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -53,3 +58,4 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
